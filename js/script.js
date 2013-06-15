6103d620fbb0a01143dd20bfd62a7a9de22f862e
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2013-06-15 22:37:55 +0200

    twitter and Facebook share

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(function() {
-
-  $("body").emailSpamProtection("email"); 
-
+    $("body").emailSpamProtection("email");
 });
+
+// Tweeter like
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+// Facebook share
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1&appId=167072150025639";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
